8fed1af2b5676e46e9dc099b7e7a8597509a2256
Kris <40231726+blackh00d@users.noreply.github.com>
2024-03-28 12:52:11 -0500

    Create vm-setup.sh

---
+++ vm-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir /mnt/hgfs
+/usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other
+
+echo "/usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other" >> ~/.bashrc
+
+apt update
+apt upgrade -y
