78de6711f63584d4e29c3953c0eb6e3e57d977cf
Kris <40231726+blackh00d@users.noreply.github.com>
2024-04-07 04:12:58 -0500

    Update vm-setup.sh

--- vm-setup.sh
+++ vm-setup.sh
@@ -3,8 +3,12 @@
 mkdir /mnt/hgfs
 /usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other
 
-echo >> ~/.profile
-echo "/usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other" >> ~/.profile
+echo >> ~/.bashrc
+echo "/usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other" >> ~/.bashrc
+
+echo >> /home/kali/.bashrc
+echo "/usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other" >> /home/kali/.bashrc
 
 apt update
 apt upgrade -y
+
